b7e0142a71e53258a77c1f214e3f10901fbd7cfb
kethomassen <kethomassen@gmail.com>
2019-04-21 12:40:22 +1000

    Added some options

--- cask-migrator.sh
+++ cask-migrator.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
+forcing=false
+remove=false
+
+searchdir="/Applications"
+installdir="/Applications"
+
+while getopts ":d:id:fhr" opt; do
+    case ${opt} in
+        h|:|\? )
+            echo "Options"
+            echo "  -f          Don't ask for confirmation for each application (Probably a bad idea)."
+            echo "  -r          Permanently remove old applications. By default just moves to trash"
+            echo "  -d [dir]    Search directory for .app files. Default: /Applications" 
+            echo "  -id [dir]   Install directory for cask. Default: /Applications"
+            echo "  -h          Print this help message and exit."
+            exit 0
+            ;;
+        f )
+            forcing=true
+            ;;
+        r )
+            remove=true
+            ;;
+        d )
+            dir=$OPTARG
+            ;;
+        id )
+            installdir=$OPTARG
+            ;;
+    esac
+done
+shift $(( OPTIND - 1))
+
 # store list of installed casks for fast access
 installed=$(brew cask list)
 
-for filename in /Applications/*.app; do
+for filename in $searchdir/*.app; do
     # extract appname from filename
     appname="$(basename "$filename" .app | tr '[:upper:]' '[:lower:]' | tr ' ' '-')"
 
@@ -20,16 +53,22 @@ for filename in /Applications/*.app; do
         continue
     fi
 
-    # ask if they want to use it
-    read -p "Able to re-install $filename as $appname via cask. Proceed? (Y/N) " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        echo "installing $appname ..."
-    
-        mv $filename ~/.Trash
-        brew cask install $appname
+    # ask for confirmation if not in force mode
+    if ! $forcing; then
+        read -p "Able to re-install $filename as $appname via cask. Proceed? (Y/N) " -n 1 -r
+        echo
+        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+            continue
+        fi
     fi
 
+    echo "Installing $appname..."
+    if $remove; then
+        rm $filename
+    else
+        mv $filename ~/.Trash
+    fi
 
+    brew cask install $appname --appdir=$installdir
 
 done
